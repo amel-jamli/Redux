690e4ac3f3cae9f6e3fd63be5ae434726590bae8
jamliamel <77271028+amel-jamli@users.noreply.github.com>
2021-03-09 10:28:29 +0100

    Add files via upload

---
+++ src/component/addTodu.js
@@ -0,0 +1,36 @@
+import React , {useState} from 'react';
+import {connect} from "react-redux";
+import {ADDTODU} from '../redux/action/action';
+import { Button, Modal, InputGroup, FormControl, Form } from 'react-bootstrap';
+
+const AddTodu = ({ADDTODU}) => {
+
+
+    const [newtext , setNewtext] = useState ("")
+
+
+
+    const handelAdd =(e) => {
+        e.preventDefault()
+        const newTodu = {
+            text : newtext ,
+            id : Math.random()
+        }
+        ADDTODU(newTodu)
+        console.log (newtext)
+        setNewtext("")
+    } 
+
+
+
+    return (
+        <div className="prin-comp">
+            <Form.Group>
+                <Form.Control id="add-btn" type="text" placeholder="enter your Todo" value={newtext} onChange={(e) => setNewtext(e.target.value)} />
+            </Form.Group>
+            <Button className="btn" variant="warning" type="submit" className="mr-2" onClick={ handelAdd}>ADD Todu</Button>
+        </div>
+    );
+}
+
+export default connect(null, { ADDTODU })(AddTodu);
---
+++ src/redux/action/action.js
@@ -0,0 +1,24 @@
+import { addtodu , deletetodu , edittodu  } from "./actionType";
+
+ export function ADDTODU (newTodu){
+    return {
+        type : addtodu ,
+        payload : newTodu
+    }
+}
+
+export function DELETETODU (edittodu){
+   return {
+       type : deletetodu ,
+       payload : edittodu
+    }
+}
+
+ export function EDITTODU (id){
+    return {
+        type : edittodu ,
+        payload : id
+    }
+}
+
+export default (ADDTODU , DELETETODU , EDITTODU )
